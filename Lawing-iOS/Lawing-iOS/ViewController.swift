e68409d4e0c180be9026307c42a96c8c5f3e6279
JoHyerin <gpfls468@gmail.com>
2024-05-08 17:10:47 +0900

    [Add] 프로젝트 파일 생성

---
+++ Lawing-iOS/Lawing-iOS/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Lawing-iOS
+//
+//  Created by 조혜린 on 5/8/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = .red
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
