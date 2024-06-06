79dcb30b302d6152118910717cee74de6fe253e7
JoHyerin <gpfls468@gmail.com>
2024-06-06 17:10:51 +0900

    [Add] #12 - LicenseAPIService 추가

---
+++ Lawing-iOS/Lawing-iOS/Network/License/LicenseAPIService.swift
@@ -0,0 +1,33 @@
+//
+//  LicenseAPIService.swift
+//  Lawing-iOS
+//
+//  Created by 조혜린 on 6/6/24.
+//
+
+import Foundation
+
+import Moya
+
+final class LicenseAPIService: BaseService {
+    static let shared = LicenseAPIService()
+    private var lisenseProvider = MoyaProvider<BaseTargetType>(plugins: [MoyaPlugin()])
+    private override init() {}
+}
+
+extension LicenseAPIService {
+    func postLisenceValid(request: LisenceValidRequestDTO, completion: @escaping (NetworkResult<LisenceResponseDTO>) -> Void) {
+        lisenseProvider.request(.postLisenceValid(request: request)) { result in
+            switch result {
+            case .success(let response):
+                let networkResult: NetworkResult<LisenceResponseDTO> = self.fetchNetworkResult(
+                    statusCode: response.statusCode,
+                    data: response.data
+                )
+                completion(networkResult)
+            case .failure:
+                completion(.networkFail)
+            }
+        }
+    }
+}
