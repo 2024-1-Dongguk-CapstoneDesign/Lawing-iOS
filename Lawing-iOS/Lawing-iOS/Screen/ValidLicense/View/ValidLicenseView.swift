d10b37066d0709e6187aa3433fbc10126519127d
JoHyerin <gpfls468@gmail.com>
2024-06-11 15:47:21 +0900

    [Feat] #17 - 유효성 검증 화면 UI 분기처리

--- Lawing-iOS/Lawing-iOS/Screen/ValidLicense/View/ValidLicenseView.swift
+++ Lawing-iOS/Lawing-iOS/Screen/ValidLicense/View/ValidLicenseView.swift
@@ -11,14 +11,23 @@ import SnapKit
 import Then
 
 final class ValidLicenseView: UIView {
+    
+    //MARK: - Properties
+    
+    typealias StartLawingButtonAction = () -> Void
+    typealias RetryButtonAction = () -> Void
+
+    private var startLawingButtonAction: StartLawingButtonAction?
+    private var retryButtonAction: RetryButtonAction?
 
     //MARK: - UI Properties
     
-    private let discriptionLabel = UILabel()
+    private let descriptionLabel = UILabel()
     private let stateImageView = UIImageView()
     private let errorMessegeLabel = UILabel()
     private let startLawingButton = UIButton()
-    
+    private let retryButton = UIButton()
+
     // MARK: - Life Cycle
     
     override init(frame: CGRect) {
@@ -36,12 +45,25 @@ final class ValidLicenseView: UIView {
 
 extension ValidLicenseView {
     
+    //MARK: - TargetView Method
+    
+    func setupStartLawingButton(action: @escaping StartLawingButtonAction) {
+        startLawingButtonAction = action
+        startLawingButton.addTarget(self, action: #selector(startLawingButtonTapped), for: .touchUpInside)
+    }
+    
+    func setupRetryButton(action: @escaping RetryButtonAction) {
+        retryButtonAction = action
+        retryButton.addTarget(self, action: #selector(retryButtonTapped), for: .touchUpInside)
+    }
+    
     // MARK: - Private Method
     
     private func setupStyle() {
         self.backgroundColor = .lawingWhite
         
-        discriptionLabel.do {
+        descriptionLabel.do {
+            $0.text = "운전 면허증의 유효성을\n검사하는 중입니다..."
             $0.numberOfLines = 0
             $0.font = .titleSemiBold
             $0.textColor = .lawingBlack
@@ -49,7 +71,7 @@ extension ValidLicenseView {
         }
         
         stateImageView.do {
-            $0.backgroundColor = .lawingGray1
+            $0.image = .check
         }
         
         errorMessegeLabel.do {
@@ -64,20 +86,31 @@ extension ValidLicenseView {
             $0.setTitleColor(.lawingBlack, for: .normal)
             $0.makeRounded(radius: 15)
             $0.titleLabel?.font = .button1Bold
+            $0.isHidden = true
+        }
+        
+        retryButton.do {
+            $0.backgroundColor = .lawingGray1
+            $0.setTitle("재시도하기", for: .normal)
+            $0.setTitleColor(.lawingBlack, for: .normal)
+            $0.makeRounded(radius: 15)
+            $0.titleLabel?.font = .button1Bold
+            $0.isHidden = true
         }
     }
     
     private func setupHierarchy() {
         addSubviews(
-            discriptionLabel,
+            descriptionLabel,
             stateImageView,
             errorMessegeLabel,
+            retryButton,
             startLawingButton
         )
     }
     
     private func setupLayout() {
-        discriptionLabel.snp.makeConstraints {
+        descriptionLabel.snp.makeConstraints {
             $0.centerX.equalToSuperview()
             $0.bottom.equalTo(stateImageView.snp.top).offset(-36)
         }
@@ -97,5 +130,36 @@ extension ValidLicenseView {
             $0.bottom.equalTo(safeAreaLayoutGuide).inset(23)
             $0.height.equalTo(56)
         }
+        
+        retryButton.snp.makeConstraints {
+            $0.horizontalEdges.equalToSuperview().inset(29)
+            $0.bottom.equalTo(safeAreaLayoutGuide).inset(23)
+            $0.height.equalTo(56)
+        }
+    }
+    
+    // MARK: - Method
+    
+    func setupUIWithValidState(state: Bool, errorMessege: String) {
+        if state {
+            descriptionLabel.text  = "운면 면허증 등록이\n완료되었습니다!"
+            stateImageView.image = .success
+            startLawingButton.isHidden = false
+        } else {
+            descriptionLabel.text  = "운면 면허증을\n등록할 수 없습니다!"
+            retryButton.isHidden = false
+            stateImageView.image = .fail
+            errorMessegeLabel.text = errorMessege
+        }
+    }
+    
+    //MARK: - @Objc Method
+
+    @objc private func startLawingButtonTapped() {
+        startLawingButtonAction?()
+    }
+    
+    @objc private func retryButtonTapped() {
+        retryButtonAction?()
     }
 }
--- Lawing-iOS/Lawing-iOS/Screen/ValidLicense/ViewController/ValidLicenseViewController.swift
+++ Lawing-iOS/Lawing-iOS/Screen/ValidLicense/ViewController/ValidLicenseViewController.swift
@@ -13,7 +13,20 @@ final class ValidLicenseViewController: UIViewController {
     
     private let rootView = ValidLicenseView()
     
+    private var validResult = false
+    private var errorMessege = ""
+    
     // MARK: - Life Cycle
+    
+    init(result: Bool, description: String) {
+        self.validResult = result
+        self.errorMessege = description
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
     override func loadView() {
         super.loadView()
@@ -24,6 +37,17 @@ final class ValidLicenseViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationController?.isNavigationBarHidden = false
+        setupTarget()
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(2)) {
+            self.rootView.setupUIWithValidState(state: self.validResult, errorMessege: self.fetcherrorMessege())
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+
+        navigationController?.isNavigationBarHidden = true
     }
 }
 
@@ -31,5 +55,32 @@ extension ValidLicenseViewController {
     
     // MARK: - Private Method
     
+    private func setupTarget() {
+        rootView.setupStartLawingButton(action: startLawingButtonTapped)
+        rootView.setupRetryButton(action: retryButtonTapped)
+    }
     
+    private func fetcherrorMessege() -> (String) {
+        switch errorMessege {
+        case "LICENSE_MEMBER_INFO_NOT_CORRECT":
+            return "회원 정보와 일치하지 않는 운전 면허증입니다."
+        case "DRIVERS_LICENSE_EXISTS":
+            return "이미 등록된 운전면허증입니다."
+        case "LICENSE_OCR_FAILED":
+            return "운전면허증 텍스트 추출을 실패하였습니다."
+        case "LICENSE_VALIDATION_FAILED":
+            return "유효하지 않은 운전면허증입니다."
+        default:
+            return ""
+        }
+    }
+    
+    private func startLawingButtonTapped() {
+        let mainVC = MainViewController()
+        navigationController?.pushViewController(mainVC, animated: true)
+    }
+    
+    private func retryButtonTapped() {
+        navigationController?.popViewController(animated: true)
+    }
 }
